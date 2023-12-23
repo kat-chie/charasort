5b5b0ad50106075b993a4be32cf4824b71d64d44
kat-chie <154698052+kat-chie@users.noreply.github.com>
2023-12-24 03:44:05 +0800

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -20,4 +20,4 @@ let dataSetVersion = '';
 /**
  * Image root, will be appended to the start of every image URL.
  */
-const imageRoot = 'https://github.com/kat-chie/charasort/tree/master/src/assets/chars/';
+const imageRoot = 'https://raw.githubusercontent.com/kat-chie/charasort/master/src/assets/chars/';
